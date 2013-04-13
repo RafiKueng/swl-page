84c4b758257e232672ee485d5e576c2fafad77c3
RafiKueng <rafi.kueng@gmx.ch>
2013-04-13 18:49:54 +0200

    img overlay

--- js/main.js
+++ js/main.js
@@ -1,1 +1,70 @@
+/*
+$(".tool").each(function(){
+  $t = $(this);
+  
+  $t.click(function(){
+    $(this).next().slideToggle();
+  })
+  .next().click(function(){
+    $(this).slideToggle();
+  })
+  
+})
 
+*/
+
+
+
+/*
+ * prevent jumpy jquery slideToggle with this workaround:
+ * http://stackoverflow.com/questions/1335461/jquery-slide-is-jumpy
+ * 
+ */
+$('.tool').click(function(event) {
+  //event.preventDefault();
+  xToggleHeight($(this).next());
+});
+$('.toolinfo').click(function(event) {
+  event.preventDefault();
+  xToggleHeight($(this).parent());
+});
+
+//For each collapsible element.
+$('.toolinfo').each(function() {
+  //Wrap a div around and set to hidden.
+  $(this).wrap('<div style="height:0;overflow:hidden;visibility:hidden;"/>');
+});
+
+function xToggleHeight(el){
+  //Get the height of the content including any margins.
+  var contentHeight = el.children('.toolinfo').outerHeight(true);
+  //If the element is currently expanded.
+  if(el.hasClass("expanded")){
+    //Collapse
+    el.removeClass("expanded")
+      .stop().animate({height:0},400,
+      function(){
+        //on collapse complete
+        //Set to hidden so content is invisible.
+        $(this).css({'overflow':'hidden', 'visibility':'hidden'});
+      }
+    );
+  }else{
+    //Expand
+    el.addClass("expanded").css({'overflow':'', 'visibility':'visible'})
+      .stop().animate({height: contentHeight},400,
+      function(){
+              //on expanded complete
+              //Set height to auto incase browser/content is resized afterwards.
+              $(this).css('height','');
+      }
+    );
+  }
+}
+
+
+/*
+$(".buttons li").click(function(){
+        location.href = $(this).find("a").attr("href");
+});
+*/
--- tools.php
+++ tools.php
@@ -1,6 +1,122 @@
+<?php
+/**
+ * Clean comments of json content and decode it with json_decode().
+ * Work like the original php json_decode() function with the same params
+ *
+ * http://php.net/manual/en/function.json-decode.php
+ * 
+ * @param   string  $json    The json string being decoded
+ * @param   bool    $assoc   When TRUE, returned objects will be converted into associative arrays.
+ * @param   integer $depth   User specified recursion depth. (>=5.3)
+ * @param   integer $options Bitmask of JSON decode options. (>=5.4)
+ * @return  string
+ */
+function json_clean_decode($json, $assoc = false, $depth = 512, $options = 0) {
+
+    // search and remove comments like /* */ and //
+    $json = preg_replace("#(/\*([^*]|[\r\n]|(\*+([^*/]|[\r\n])))*\*+/)|([\s\t](//).*)#", '', $json);
+
+    if(version_compare(phpversion(), '5.4.0', '>=')) {
+        $json = json_decode($json, $assoc, $depth, $options);
+    }
+    elseif(version_compare(phpversion(), '5.3.0', '>=')) {
+        $json = json_decode($json, $assoc, $depth);
+    }
+    else {
+        $json = json_decode($json, $assoc);
+    }
+
+    return $json;
+}
+
+
+/**
+ * reads the /tools/ folder and creates a tools entry for each *.json file
+ */
+
+$dir = "./tools/*.json"; // dir to the tools json files
+$files = glob($dir);
+
+//echo var_dump($files) . '<br/>';
+
+foreach($files as $file)
+{
+  
+  $t = json_clean_decode(file_get_contents($file));
+  //echo var_dump($t) . '<br/>';
+
+  if (property_exists($t, 'ScreenshotURL')) {
+    if ($t->ScreenshotURL=='') {$t->ScreenshotURL = 'tools/noscr.png';}
+    else if (file_exists('tools/' . $t->ScreenshotURL)) {$t->ScreenshotURL = 'tools/' . $t->ScreenshotURL;}
+  }
+  else {$t->ScreenshotURL = 'tools/noscr.png';}
+
+
+  if (property_exists($t, 'BannerURL')) {
+    if ($t->BannerURL=='') {$t->BannerURL = 'tools/noimg.png';}
+    else if (file_exists('tools/' . $t->BannerURL)) {$t->BannerURL = 'tools/' . $t->BannerURL;}
+  }
+  else {$t->BannerURL = 'tools/noimg.png';}
+
+
+  //compose the action menue
+  $a = '';
+  if ($t->HelpURL !== '') {$a=$a."      <li class='hlp'><a href='".$t->UseItURL."'>view help page</a></li>\n";}
+  if ($t->ScreencastURL !== '') {$a=$a."      <li class='scr'><a href='".$t->UseItURL."'>view demonstration / screencast</a></li>\n";}
+  if ($t->SourcesURL !== '') {$a=$a."      <li class='src'><a href='".$t->UseItURL."'>view sources</a></li>\n";}
+  
+  
+  echo "
+<div class='tool' style='background-image: url(" . $t->BannerURL . ");'>
+  <h3>".$t->Name ."</h3>
+  <p>". $t->ShortDescr ."</p>
+</div>
+<div class='toolinfo'>
+  <div class='screenshot'><img src=".$t->ScreenshotURL." alt='Screenshot' width='380' /> </div>
+  <div class='buttons'>
+    <ul>
+      <li class='use'><a href='".$t->UseItURL."'>use it</a></li>\n"
+      . $a . "
+    </ul>
+  </div>
+  <div class='description'>" . $t->LongDescr . "</div>
+  <div class='end'></div>
+</div>
+";
+}
+
+?>
+
+
+<!-- overlay for screenshot display -->
+<div id="overlay">
+  <div id="overlaycont">
+    <img id="overlayimg" src="img/noscr.png" alt=""/>
+  </div>
+</div>
+
+
+
+<!--
+
+
+
+
 <div id="nr2" class="tool">
   <h3>LensToy</h3>
   <p>A gravitational lens simulator in Javascript/HTML5</p>
+</div>
+<div class="toolinfo">
+  <div class='screenshot'>a screenshot</div>
+  <div class='buttons'>
+    <ul>
+      <li class='use'>use it</li>
+      <li class='demo'>view demo / help</li>
+      <li class='src'>view source code</li>
+    </ul>
+  </div>
+  <div class='description'>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</div>
+  <div class="end"></div>
 </div>
 
 <div id="nr3" class="tool">
@@ -24,13 +140,52 @@
   <h3>LensA</h3>
   <p>Some one line description</p>
 </div>
+<div class="toolinfo">
+  <div class='screenshot'>a screenshot</div>
+  <div class='buttons'>
+    <ul>
+      <li class='use'>use it</li>
+      <li class='demo'>view demo / help</li>
+      <li class='src'>view source code</li>
+    </ul>
+  </div>
+  <div class='description'>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</div>
+  <div class="end"></div>
+</div>
 
 <div id="nr1" class="tool">
   <h3>SpaghettiLens</h3>
   <p>Some one line description</p>
+</div>
+<div class="toolinfo">
+  <div class='screenshot'>a screenshot</div>
+  <div class='buttons'>
+    <ul>
+      <li class='use'>use it</li>
+      <li class='demo'>view demo / help</li>
+      <li class='src'>view source code</li>
+    </ul>
+  </div>
+  <div class='description'>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</div>
+  <div class="end"></div>
 </div>
 
 <div id="forum" class="tool forum">
   <h3>Forum</h3>
   <p>Discuss models</p>
 </div>
+<div class="toolinfo">
+  <div class='screenshot'>a screenshot</div>
+  <div class='buttons'>
+    <ul>
+      <li class='use'>use it</li>
+      <li class='demo'>view demo / help</li>
+      <li class='src'>view source code</li>
+    </ul>
+  </div>
+  <div class='description'>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</div>
+  <div class="end"></div>
+</div>
+
+
+-->
